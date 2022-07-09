05eb010b486ef5291f73cb0c57ed6d8651301dbb
callistopan <callistopan@gmail.com>
2022-07-09 16:27:20 +0530

    Astar algorithm

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
-import Node from './Node/Node';
-import { dijkstra, getNodesInShortestPathOrder } from '../algorithms/dijkstra';
-import { dfs ,getNodesInShortestPathOrder_dfs } from '../algorithms/dfs';
-import { bfs } from '../algorithms/bfs';
+import React, { Component } from "react";
+import Node from "./Node/Node";
+import { dijkstra, getNodesInShortestPathOrder } from "../algorithms/dijkstra";
+import { dfs, getNodesInShortestPathOrder_dfs } from "../algorithms/dfs";
+import { bfs } from "../algorithms/bfs";
+import { Astar } from "../algorithms/astar";
 
-import './PathfindingVisualizer.css';
+import "./PathfindingVisualizer.css";
 
 // randomize the start and finish nodes
 const START_NODE_ROW = Math.floor(Math.random() * 20);
 
-const START_NODE_COL =  Math.floor(Math.random() * 50);
-const FINISH_NODE_ROW = Math.floor(Math.random() * 20);   
-const FINISH_NODE_COL =     Math.floor(Math.random() * 50);
-
-
-
+const START_NODE_COL = Math.floor(Math.random() * 50);
+const FINISH_NODE_ROW = Math.floor(Math.random() * 20);
+const FINISH_NODE_COL = Math.floor(Math.random() * 50);
 
 export default class PathfindingVisualizer extends Component {
   constructor() {
@@ -25,97 +23,121 @@ export default class PathfindingVisualizer extends Component {
     };
   }
 
-  componentDidMount() {   
-    const grid = getInitialGrid();  // initialize the grid and update the state
-    this.setState({grid});
+  componentDidMount() {
+    const grid = getInitialGrid(); // initialize the grid and update the state
+    this.setState({ grid });
   }
-
 
   // mouse down event handler
   handleMouseDown(row, col) {
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col); // get the new grid with the wall toggled
-    this.setState({grid: newGrid, mouseIsPressed: true}); // update the state with the new grid and mouseIsPressed
+    this.setState({ grid: newGrid, mouseIsPressed: true }); // update the state with the new grid and mouseIsPressed
   }
 
   // mouse up event handler
   handleMouseEnter(row, col) {
-    if (!this.state.mouseIsPressed) return;   // if the mouse is not pressed, return
+    if (!this.state.mouseIsPressed) return; // if the mouse is not pressed, return
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col); // get the new grid with the wall toggled
-    this.setState({grid: newGrid}); // update the state with the new grid
+    this.setState({ grid: newGrid }); // update the state with the new grid
   }
 
   // mouse up event handler
   handleMouseUp() {
-    this.setState({mouseIsPressed: false}); // update the state with the mouseIsPressed as false
+    this.setState({ mouseIsPressed: false }); // update the state with the mouseIsPressed as false
   }
 
-
-
-
-  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {  // animate the dijkstra algorithm
+  animateAlgorithm(visitedNodesInOrder, nodesInShortestPathOrder) {
+    // animate the dijkstra algorithm
 
     for (let i = 0; i <= visitedNodesInOrder.length; i++) {
-      if (i === visitedNodesInOrder.length) { // if the i is equal to the visited nodes in order length,
-        setTimeout(() => {  // set a timeout to animate the shortest path
+      if (i === visitedNodesInOrder.length) {
+        // if the i is equal to the visited nodes in order length,
+        setTimeout(() => {
+          // set a timeout to animate the shortest path
           this.animateShortestPath(nodesInShortestPathOrder);
         }, 10 * i);
         return;
       }
 
-      setTimeout(() => {  // set the timeout for the animation
-        const node = visitedNodesInOrder[i];    // get the node from the visited nodes in order
+      setTimeout(() => {
+        // set the timeout for the animation
+        const node = visitedNodesInOrder[i]; // get the node from the visited nodes in order
         document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-visited';  // update the node class name to node-visited
+          "node node-visited"; // update the node class name to node-visited
       }, 10 * i); // set the timeout for the animation
     }
   }
 
-
-
-  animateShortestPath(nodesInShortestPathOrder) { // animate the shortest path
-    for (let i = 0; i < nodesInShortestPathOrder.length; i++) { // for each node in the shortest path order
-      setTimeout(() => {  // set the timeout for the animation
+  animateShortestPath(nodesInShortestPathOrder) {
+    // animate the shortest path
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      // for each node in the shortest path order
+      setTimeout(() => {
+        // set the timeout for the animation
         const node = nodesInShortestPathOrder[i]; // get the node from the shortest path order
-        document.getElementById(`node-${node.row}-${node.col}`).className = 
-          'node node-shortest-path';  // update the node class name to node-shortest-path
-      }, 50 *i ); // set the timeout for the animation
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-shortest-path"; // update the node class name to node-shortest-path
+      }, 50 * i); // set the timeout for the animation
     }
   }
 
-
-
-  visualizeDijkstra() {
-    const {grid} = this.state;    // get current grid from the state
+  visualizeAlgorithms() {
+    const { grid } = this.state; // get current grid from the state
     const startNode = grid[START_NODE_ROW][START_NODE_COL]; // get the start node from the grid
-    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];  // get the finish node from the grid
-    const visitedNodesInOrder = dfs(grid, startNode, finishNode);  // get the visited nodes in order from the dijkstra algorithm
-    
-    const nodesInShortestPathOrder = getNodesInShortestPathOrder_dfs(finishNode); // get the nodes in shortest path order from the finish node
-    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);  // animate the dijkstra algorithm
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL]; // get the finish node from the grid
+    const algorithm = document.getElementById("algorithm").value; // get the algorithm from the dropdown menu
+    // switch statement to call the appropriate algorithm
+    let visitedNodesInOrder =[]
+    switch (algorithm) {
+      case "bfs":
+        visitedNodesInOrder = bfs(grid, startNode, finishNode);
+        break;
+      case "dfs":
+        visitedNodesInOrder = dfs(grid, startNode, finishNode);
+        break;
+      case "dijkstra":
+        visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+        break;
+      case "astar":
+        visitedNodesInOrder = Astar(grid, startNode, finishNode);
+        break;
+      default:
+        break;
+    }
+
+    const nodesInShortestPathOrder =
+      getNodesInShortestPathOrder_dfs(finishNode); // get the nodes in shortest path order from the finish node
+    this.animateAlgorithm(visitedNodesInOrder, nodesInShortestPathOrder); // animate the dijkstra algorithm
   }
+
+  reset() {
+    // refresh the page
+    window.location.reload();
     
-    
-
-
-
+  }
 
   render() {
-    const { grid, mouseIsPressed } = this.state;  // get the grid and mouseIsPressed from the state
-   
+    const { grid, mouseIsPressed } = this.state; // get the grid and mouseIsPressed from the state
 
     return (
       <>
-        <button onClick={() => this.visualizeDijkstra()}>
-          Visualize Dijkstra's Algorithm
-        </button>
+        <select name="algorithm" id="algorithm">
 
+          <option value="bfs">BFS</option>
+          <option value="dfs">DFS</option>
+          <option value="dijkstra">Dijkstra</option>
+          <option value="astar">A*</option>
+        </select>
+
+        <button onClick={() => this.visualizeAlgorithms()}>Visualize</button>
+        <button onClick={()=> this.reset()}>Reset</button>
 
         <div className="grid">
           {grid.map((row, rowIdx) => {
             return (
               <div key={rowIdx}>
                 {row.map((node, nodeIdx) => {
-                  const {row, col, isFinish, isStart, isWall} = node; // get the row, col, isFinish, isStart, and isWall from the node object
+                  const { row, col, isFinish, isStart, isWall } = node; // get the row, col, isFinish, isStart, and isWall from the node object
                   return (
                     <Node
                       key={nodeIdx}
@@ -126,9 +148,11 @@ export default class PathfindingVisualizer extends Component {
                       isWall={isWall}
                       mouseIsPressed={mouseIsPressed}
                       onMouseDown={(row, col) => this.handleMouseDown(row, col)}
-                      onMouseEnter={(row, col) =>this.handleMouseEnter(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
                       onMouseUp={() => this.handleMouseUp()}
-                      ></Node>
+                    ></Node>
                   );
                 })}
               </div>
@@ -140,12 +164,9 @@ export default class PathfindingVisualizer extends Component {
   }
 }
 
-
-
-
 const getInitialGrid = () => {
-  const grid = []; 
-  for (let row = 0; row < 20; row++) {  
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
     const currentRow = [];
     for (let col = 0; col < 50; col++) {
       currentRow.push(createNode(col, row));
@@ -155,16 +176,16 @@ const getInitialGrid = () => {
   return grid;
 };
 
-
-const createNode = (col, row) => { // create a node object
+const createNode = (col, row) => {
+  // create a node object
   return {
-    col,    // column
-    row,    // row
-    isStart: row === START_NODE_ROW && col === START_NODE_COL,  // if the node is the start node
+    col, // column
+    row, // row
+    isStart: row === START_NODE_ROW && col === START_NODE_COL, // if the node is the start node
     isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL, // if the node is the finish node
     distance: Infinity, // distance from start node
     isVisited: false, // if the node has been visited
-    isWall: false,  // if the node is a wall
+    isWall: false, // if the node is a wall
     previousNode: null, // previous node of this node
   };
 };
@@ -173,10 +194,11 @@ const createNode = (col, row) => { // create a node object
 const getNewGridWithWallToggled = (grid, row, col) => {
   const newGrid = grid.slice(); // copy the grid
   const node = newGrid[row][col]; // get the node
-  const newNode = { // create a new node
-    ...node,  // copy the properties of the old node
+  const newNode = {
+    // create a new node
+    ...node, // copy the properties of the old node
     isWall: !node.isWall, // toggle the wall property
   };
-  newGrid[row][col] = newNode;  //  update the grid
-  return newGrid;   // return the new grid
+  newGrid[row][col] = newNode; //  update the grid
+  return newGrid; // return the new grid
 };
---
+++ src/algorithms/astar.js
@@ -0,0 +1,107 @@
+// a star algorithm for finding the shortest path
+
+
+export function Astar(grid, startNode, finishNode) {
+    // closedList is 2d array of boolean
+    let visitedNodesInorder=[]
+    let closedList = new Array(grid.length);
+    for (let i = 0; i < grid.length; i++) {
+        closedList[i] = new Array(grid[0].length);
+    }
+    // set each node to false
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            closedList[i][j] = false;
+        }
+    }
+    // node details
+    let nodeDetails = new Array(grid.length);
+    for (let i = 0; i < grid.length; i++) {
+        nodeDetails[i] = new Array(grid[0].length);
+    }
+    // set each node to to an object
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            nodeDetails[i][j] = {
+                f: 10000,
+                g: 10000,
+                h: 10000,
+                previousNode: null
+            }
+        }
+    }
+    // set the start node to 0
+    nodeDetails[startNode.row][startNode.col].g = 0;
+    nodeDetails[startNode.row][startNode.col].h = 0;
+    nodeDetails[startNode.row][startNode.col].f = 0;
+
+    //openlist
+    let openList = [];
+    openList.push(startNode);
+
+
+    while (openList.length) {
+        console.log('hi')
+
+        // choose in openlist having lowest f score
+        let currentNode = openList[0];
+        
+        for (let i = 0; i < openList.length; i++) {
+            if (nodeDetails[openList[i].row][openList[i].col].f < nodeDetails[currentNode.row][currentNode.col].f) {
+                currentNode = openList[i];
+            }
+        }
+        if (currentNode === finishNode) {
+            console.log(visitedNodesInorder)
+            return visitedNodesInorder;
+        }
+        // remove current node from openlist
+        openList.splice(openList.indexOf(currentNode), 1);
+        // add current node to closedlist
+        closedList[currentNode.row][currentNode.col] = true;
+
+        currentNode.isVisited = true;
+        visitedNodesInorder.push(currentNode);
+        // get unvisited neighbors of current node
+        let unvisitedNeighbors = getUnvisitedNeighbors(currentNode, grid);
+
+        // for each neighbor
+        for (const neighbor of unvisitedNeighbors) {
+            
+
+            //update the g score
+            let gScore = nodeDetails[currentNode.row][currentNode.col].g + 1;
+            if (gScore < nodeDetails[neighbor.row][neighbor.col].g) {
+                neighbor.previousNode = currentNode;
+                nodeDetails[neighbor.row][neighbor.col].g = gScore;
+                nodeDetails[neighbor.row][neighbor.col].h= heuristic(neighbor, finishNode);
+                nodeDetails[neighbor.row][neighbor.col].f = nodeDetails[neighbor.row][neighbor.col].g + nodeDetails[neighbor.row][neighbor.col].h;
+                // if neighbor is not in openlist, add it to openlist
+                if (!openList.includes(neighbor)) {
+                    console.log("neighbor")
+                    if (!neighbor.isWall) {
+                        openList.push(neighbor);
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    return visitedNodesInorder;
+
+}
+
+function getUnvisitedNeighbors(node, grid) {
+    const neighbors = []
+    const { col, row } = node
+    if (row > 0) neighbors.push(grid[row - 1][col])
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col])
+    if (col > 0) neighbors.push(grid[row][col - 1])
+    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1])
+    return neighbors.filter((neighbor) => !neighbor.isVisited)
+}
+
+function heuristic(node, finishNode) {
+    return Math.abs(node.row - finishNode.row) + Math.abs(node.col - finishNode.col);
+}
